7f008a394ebfa04dd1375d5a73f2b3520f883d90
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-09-19 17:37:22 -0300

    refactor: remove unused express paths

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,67 +1,30 @@
-import { eq } from 'drizzle-orm';
-import express from 'express';
-import 'express-async-errors';
-import httpStatus from 'http-status';
+import { eq } from "drizzle-orm";
+import express from "express";
+import "express-async-errors";
+import httpStatus from "http-status";
 
-import { db } from '../drizzle/index';
-import { redirects } from '../drizzle/schema/redirects';
-import { decodeJWT, encodeJWT } from '../utils/JWTRoutes';
-import 'dotenv/config';
+import { db } from "../drizzle/index";
+import { redirects } from "../drizzle/schema/redirects";
+import { decodeJWT } from "../utils/JWTRoutes";
+import "dotenv/config";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+/**
+ * Root path
+ */
+router.get("/", (_, res) => {
   res.status(httpStatus.OK).json({
-    message: 'Home',
+    message: "hack the planet",
   });
 });
 
-router.get('/routes', async (req, res) => {
+/**
+ * Redirect path
+ */
+router.get("/jwt/:jwt", async (req, res) => {
   try {
-    const result = await db
-      .select()
-      .from(redirects)
-
-    const BASE_HOST = `${req.protocol}://${req.get('host')}`;
-    const buildLink = (jwt: Record<string, any>) => `${BASE_HOST}/jwt/${encodeJWT({ uuid: jwt.uuid })}`;
-    const ans = result.map(row => ({
-      ...row,
-      link: buildLink(row)
-    }))
-
-    res.status(httpStatus.OK).json(ans)
-  } catch (error) {
-
-  }
-})
-
-router.get('/redirect/:uuid', async (req, res) => {
-  try {
-    const { uuid } = req.params;
-
-    const result = await db
-      .select()
-      .from(redirects)
-      .where(eq(redirects.uuid, uuid));
-
-    if (result.length === 0) {
-      return res.status(httpStatus.NOT_FOUND).json({
-        message: 'Redirect not found',
-      });
-    }
-
-    return res.redirect(result[0].url);
-  } catch (error) {
-    console.error(error);
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      message: 'An error occurred',
-    });
-  }
-});
-
-router.get('/jwt/:jwt', async (req, res) => {
-  try {
-    const { uuid } = decodeJWT(req.params.jwt)
+    const { uuid } = decodeJWT(req.params.jwt);
 
     const result = await db
       .select()
@@ -70,7 +33,7 @@ router.get('/jwt/:jwt', async (req, res) => {
 
     if (result.length === 0) {
       return res.status(httpStatus.NOT_FOUND).json({
-        message: 'Redirect not found',
+        message: "Redirect not found",
       });
     }
 
@@ -78,15 +41,9 @@ router.get('/jwt/:jwt', async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      message: 'An error occurred',
+      message: "An error occurred",
     });
   }
-});
-
-router.get('/healthcheck', (req, res) => {
-  res.status(httpStatus.OK).json({
-    message: 'OK',
-  });
 });
 
 export default router;
