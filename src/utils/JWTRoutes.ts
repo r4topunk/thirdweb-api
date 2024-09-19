e43f9c3b8fc00a5d26c0b7aaf3a9e1cfb26fde8f
arthursiq5 <arthur.siq5@gmail.com>
2024-09-19 10:33:11 -0300

    Updated const use

--- src/utils/JWTRoutes.ts
+++ src/utils/JWTRoutes.ts
@@ -1,7 +1,5 @@
 import jwt from 'jsonwebtoken';
-import 'dotenv/config';
-
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || '';
+import { JWT_SECRET_KEY } from './Const';
 
 interface JWTCustomToken {
     uuid: string
