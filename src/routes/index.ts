e34f45faf54a69f3747d2bf11940c4d59a4eefa5
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-10-11 16:10:18 -0300

    refactor: remove drizze and jwt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,6 @@
-import { eq } from "drizzle-orm";
 import express from "express";
 import "express-async-errors";
 import httpStatus from "http-status";
-
-import { db } from "../drizzle/index";
-import { redirects } from "../drizzle/schema/redirects";
-import { decodeJWT } from "../utils/JWTRoutes";
 import "dotenv/config";
 
 const router = express.Router();
@@ -17,33 +12,6 @@ router.get("/", (_, res) => {
   res.status(httpStatus.OK).json({
     message: "hack the planet",
   });
-});
-
-/**
- * Redirect path
- */
-router.get("/jwt/:jwt", async (req, res) => {
-  try {
-    const { uuid } = decodeJWT(req.params.jwt);
-
-    const result = await db
-      .select()
-      .from(redirects)
-      .where(eq(redirects.uuid, uuid as string));
-
-    if (result.length === 0) {
-      return res.status(httpStatus.NOT_FOUND).json({
-        message: "Redirect not found",
-      });
-    }
-
-    return res.redirect(result[0].url);
-  } catch (error) {
-    console.error(error);
-    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      message: "An error occurred",
-    });
-  }
 });
 
 export default router;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,1 @@
 import 'dotenv/config';
-
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || '';
-export const DATABASE_DIRECT_URL = process.env.DATABASE_DIRECT_URL || '';
