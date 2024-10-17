eeb962bbe253f3119f7c406bbb582a088f5bc1b9
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-10-17 03:21:45 -0300

    refactor: update data output

--- src/lib/nft.ts
+++ src/lib/nft.ts
@@ -1,119 +1,23 @@
-import { getContract, sendTransaction } from "thirdweb";
-import {
-  burn,
-  claimTo,
-  getActiveClaimCondition,
-  getClaimConditions,
-  getNFT,
-  getOwnedNFTs,
-  getOwnedTokenIds,
-  isBurnSupported,
-  isClaimToSupported,
-} from "thirdweb/extensions/erc1155";
-import {
-  CHAIN,
-  CONTRACT_ADDRESS,
-  WALLET_PRIVATE_KEY,
-} from "../utils/constants";
-import { getWalletBalance, privateKeyToAccount } from "thirdweb/wallets";
-import { client } from "../utils/thirdweb";
-import { NFTMetadata } from "thirdweb/utils";
-
-// 2. get the contract
-const contract = getContract({
-  client,
-  address: CONTRACT_ADDRESS,
-  chain: CHAIN,
-});
-
-const account = privateKeyToAccount({
-  client,
-  privateKey: WALLET_PRIVATE_KEY,
-});
-const address = account.address;
+import { zoraSepolia } from "thirdweb/chains";
+import { getNFTStatus } from "../services/nftService";
+import { CONTRACT_ADDRESS } from "../utils/constants";
 
 const TEST_WALLET = "0xC1460636c95DBE88FC3FF9bdAC9Af76720089861";
 const R4TO_WALLET = "0xC68b71B6Bf78E4BE2A0229345A2C3EeBB032247F";
 
-async function logOwnedNFTs() {
-  // 3. call the extension function
-  const ownedNFTs = await getOwnedNFTs({
-    contract,
-    address: R4TO_WALLET,
-  });
-
-  console.log(ownedNFTs);
-}
-
-async function logCreateAccount() {
-  console.log("Connected as", address);
-}
-
-async function logWalletData() {
-  // Get the balance of the account
-  const balance = await getWalletBalance({
-    address,
-    chain: CHAIN,
-    client,
-  });
-  console.log("Balance:", balance.displayValue, balance.symbol);
-}
-
-async function burnNft() {
-  const transaction = burn({
-    contract,
-    account: TEST_WALLET,
-    id: 0n,
-    value: 1n,
-  });
-
-  // Send the transaction
-  try {
-    const res = await sendTransaction({ transaction, account });
-    console.log(res);
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 (async () => {
-  const tokenId = 9n;
-  
-  // console.log("teste")
-  // const conditions = await getClaimConditions({
-  //   contract,
-  //   tokenId,
-  // });
-  // console.log(conditions)
-  // const activeClaimCondition = await getActiveClaimCondition({
-  //   contract,
-  //   tokenId,
-  // });
-  // console.log(activeClaimCondition)
+  const userAddress = TEST_WALLET
+  const contractAddress = CONTRACT_ADDRESS
+  const tokenId = 9n
+  const chainId = zoraSepolia.id
 
-  // const transaction = claimTo({
-  //   contract,
-  //   to: R4TO_WALLET,
-  //   tokenId,
-  //   quantity: 1n,
-  // });
-   
-  // const tx = await sendTransaction({ transaction, account });
-  // console.log(tx)
-
-  // logOwnedNFTs();
-
-  // const ownedTokenIds = await getOwnedTokenIds({
-  //   contract,
-  //   address: R4TO_WALLET,
-  // });
-  // console.log(ownedTokenIds)
-
-  const nft = await getNFT({
-    contract,
-    tokenId: 9n,
+  const nftStatus = await getNFTStatus({
+    userAddress,
+    contractAddress,
+    tokenId,
+    chainId,
   });
 
-  console.dir(nft, { depth: null, colors: true })
-  
+  console.dir(nftStatus, {depth: null, colors: true})
 })() ;
--- src/services/nftService.ts
+++ src/services/nftService.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 import "express-async-errors";
-import { ContractOptions, defineChain, getContract, sendTransaction } from "thirdweb";
+import {
+  ContractOptions,
+  defineChain,
+  getContract,
+  sendTransaction,
+} from "thirdweb";
 import {
   claimTo,
   getClaimConditions,
@@ -10,7 +15,7 @@ import {
 import { client, account } from "../utils/thirdweb";
 
 /**
- * Interface para os parâmetros de entrada da função getNFTStatus
+ * Interface for the input parameters of the getNFTStatus function
  */
 interface GetNFTStatusParams {
   userAddress: string;
@@ -19,92 +24,133 @@ interface GetNFTStatusParams {
   chainId: number;
 }
 
-type Contract = Readonly<ContractOptions<[]>>
+type Contract = Readonly<ContractOptions<[]>>;
+
+interface NftContractData {
+  nftContract: Contract;
+  nftDetails: any;
+  claimConditions: any[];
+  userOwnedTokenIds: { tokenId: bigint; balance: bigint }[];
+  isUserOwner: boolean;
+}
 
 /**
- * Obtém o contrato NFT
+ * Fetches all data related to an NFT contract: contract instance, details, claim conditions, and user-owned token IDs.
  */
-function getNFTContract(params: { contractAddress: string; chainId: number }) {
-  const { contractAddress, chainId } = params;
-  return getContract({
+async function getNFTContractData(
+  contractAddress: string,
+  chainId: number,
+  tokenId: bigint,
+  userAddress: string
+): Promise<NftContractData> {
+  const nftContract = getContract({
     client,
     address: contractAddress,
     chain: defineChain(chainId),
   });
+
+  const [nftDetails, claimConditions, userOwnedTokenIds] = await Promise.all([
+    getNFT({
+      contract: nftContract,
+      tokenId,
+    }),
+    getClaimConditions({
+      contract: nftContract,
+      tokenId,
+    }),
+    getOwnedTokenIds({
+      contract: nftContract,
+      address: userAddress,
+    }),
+  ]);
+
+  const isUserOwner = userOwnedTokenIds.some(
+    (token) => token.tokenId === tokenId
+  );
+
+  return { nftContract, nftDetails, claimConditions, userOwnedTokenIds, isUserOwner };
 }
 
 /**
- * Obtém os detalhes do NFT
+ * Fetches all necessary data for the NFT, including contract, details, claim conditions, and user ownership.
+ *
+ * @param userAddress - Address of the user
+ * @param contractAddress - NFT contract address
+ * @param tokenId - Token ID of the NFT
+ * @param chainId - Chain ID where the contract is deployed
+ * @returns An object with all fetched NFT data
  */
-async function getNFTDetails(nftContract: Contract, tokenId: bigint) {
-  return await getNFT({
-    contract: nftContract,
-    tokenId,
-  });
-}
-
-/**
- * Obtém as condições de claim do NFT
- */
-async function getNFTClaimConditions(nftContract: Contract, tokenId: bigint) {
-  return await getClaimConditions({
-    contract: nftContract,
-    tokenId,
-  });
-}
-
-/**
- * Verifica se o NFT foi mintado
- */
-function isNFTMinted(nftDetails: any) {
-  const isERC1155 = nftDetails.type === "ERC1155";
-  return isERC1155 && nftDetails.supply > 0;
-}
-
-/**
- * Obtém os token IDs que o usuário possui
- */
-async function getUserOwnedTokenIds(nftContract: Contract, userAddress: string) {
-  return await getOwnedTokenIds({
-    contract: nftContract,
-    address: userAddress,
-  });
-}
-
-/**
- * Verifica se o usuário é proprietário do produto
- */
-function isUserOwner(
-  userOwnedTokenIds: Array<{ tokenId: bigint }>,
-  tokenId: bigint
-) {
-  return userOwnedTokenIds.some((token) => token.tokenId === tokenId);
-}
-
-/**
- * Realiza o claim do NFT
- */
-async function claimNFT(
-  nftContract: Contract,
+export const fetchNftData = async (
   userAddress: string,
-  tokenId: bigint
-) {
-  const claimTransaction = claimTo({
-    contract: nftContract,
-    to: userAddress,
-    tokenId,
-    quantity: 1n,
-  });
-
-  const transaction = await sendTransaction({
-    transaction: claimTransaction,
-    account,
-  });
-  return transaction.transactionHash;
-}
+  contractAddress: string,
+  tokenId: bigint,
+  chainId: number
+): Promise<NftContractData> => {
+  return getNFTContractData(contractAddress, chainId, tokenId, userAddress);
+};
 
 /**
- * Extrai o valor de um atributo específico
+ * Retrieves the status of an NFT for a specific user.
+ *
+ * @param params - Parameters needed to check the NFT status
+ * @returns An object containing information about the NFT status
+ */
+export const getNFTStatus = async (params: GetNFTStatusParams) => {
+  const { userAddress, contractAddress, tokenId, chainId } = params;
+
+  // Fetch the main contract data
+  const mainContractData = await fetchNftData(
+    userAddress,
+    contractAddress,
+    tokenId,
+    chainId
+  );
+
+  // Check if the NFT has POAP attributes (to determine if there's a secondary contract)
+  const nftAttributes = mainContractData.nftDetails.metadata.attributes;
+  const poapContractAddress = nftAttributes
+    ? getAttributeValue(nftAttributes, "poap_contract")
+    : null;
+  const poapTokenIdValue = nftAttributes
+    ? getAttributeValue(nftAttributes, "poap_token_id")
+    : null;
+
+  let poapContractData: NftContractData | null = null;
+
+  // If POAP contract exists, fetch its data
+  if (poapContractAddress && poapTokenIdValue) {
+    const poapTokenId = BigInt(poapTokenIdValue);
+    poapContractData = await fetchNftData(
+      userAddress,
+      poapContractAddress,
+      poapTokenId,
+      chainId
+    );
+  }
+
+  // Return full data for both the main contract and the POAP contract
+  return {
+    mainContract: {
+      nftDetails: mainContractData.nftDetails,
+      claimConditions: mainContractData.claimConditions,
+      userOwnedTokenIds: mainContractData.userOwnedTokenIds,
+      isUserOwner: mainContractData.isUserOwner,
+      transactionHash: null, // Add this if needed
+    },
+    poapContract: poapContractData
+      ? {
+          nftDetails: poapContractData.nftDetails,
+          claimConditions: poapContractData.claimConditions,
+          userOwnedTokenIds: poapContractData.userOwnedTokenIds,
+          isUserOwner: poapContractData.isUserOwner,
+          transactionHash: null, // Add this if needed
+        }
+      : null,
+  };
+};
+
+/**
+ * Extracts the value of a specific attribute
  */
 function getAttributeValue(attributes: any, traitType: string) {
   if (Array.isArray(attributes)) {
@@ -117,124 +163,3 @@ function getAttributeValue(attributes: any, traitType: string) {
   }
   return null;
 }
-
-/**
- * Lida com o claim do POAP
- */
-async function handlePoapClaim(
-  nftAttributes: any,
-  userAddress: string,
-  chainId: number,
-  client: any,
-) {
-  if (nftAttributes) {
-    const poapContractAddress = getAttributeValue(
-      nftAttributes,
-      "poap_contract"
-    );
-    const poapTokenIdValue = getAttributeValue(nftAttributes, "poap_token_id");
-
-    if (poapContractAddress && poapTokenIdValue) {
-      const poapTokenId = BigInt(poapTokenIdValue);
-
-      const poapContract = getContract({
-        client,
-        address: poapContractAddress,
-        chain: defineChain(chainId),
-      });
-
-      const poapClaimTransaction = claimTo({
-        contract: poapContract,
-        to: userAddress,
-        tokenId: poapTokenId,
-        quantity: 1n,
-      });
-
-      const poapTransaction = await sendTransaction({
-        transaction: poapClaimTransaction,
-        account,
-      });
-      return {
-        isUserProductOwner: true,
-        transactionHash: poapTransaction.transactionHash,
-      };
-    }
-  }
-  return { isUserProductOwner: false, transactionHash: null };
-}
-
-/**
- * Obtém o status de um NFT para um usuário específico.
- *
- * @param params - Parâmetros necessários para verificar o status do NFT
- * @returns Objeto contendo informações sobre o status do NFT
- */
-export const getNFTStatus = async (params: GetNFTStatusParams) => {
-  const { userAddress, contractAddress, tokenId, chainId } =
-    params;
-
-  // Inicialização das variáveis de status
-  let nftStatus: "created" | "minted" = "minted";
-  let isUserProductOwner = false;
-  let isUserPoapOwner = false;
-  let transactionHash: string | null = null;
-
-  // Obtém o contrato do produto (NFT)
-  const nftContract = getNFTContract({ contractAddress, chainId });
-
-  // Obtém os detalhes do NFT
-  const nftDetails = await getNFTDetails(nftContract, tokenId);
-
-  // Obtém as condições de claim para o NFT
-  const claimConditions = await getNFTClaimConditions(nftContract, tokenId);
-
-  // Verifica se existem condições de claim
-  if (claimConditions.length > 0) {
-    if (isNFTMinted(nftDetails)) {
-      nftStatus = "minted";
-
-      // Obtém os IDs dos tokens que o usuário possui
-      const userOwnedTokenIds = await getUserOwnedTokenIds(
-        nftContract,
-        userAddress
-      );
-
-      // Verifica se o usuário é proprietário do produto
-      isUserProductOwner = isUserOwner(userOwnedTokenIds, tokenId);
-
-      // Se o usuário não for proprietário, realiza o claim
-      if (!isUserProductOwner) {
-        transactionHash = await claimNFT(
-          nftContract,
-          userAddress,
-          tokenId,
-        );
-        isUserPoapOwner = true;
-      }
-    } else {
-      // Define o status como "created" já que o NFT não foi mintado
-      nftStatus = "created";
-
-      // Se o NFT não foi mintado, verifica se possui atributos específicos
-      const nftAttributes = nftDetails.metadata.attributes;
-
-      const poapResult = await handlePoapClaim(
-        nftAttributes,
-        userAddress,
-        chainId,
-        client,
-      );
-      isUserProductOwner = poapResult.isUserProductOwner;
-      transactionHash = poapResult.transactionHash;
-    }
-  }
-
-  // Retorna o status do NFT
-  return {
-    claimConditions,
-    nftStatus,
-    isUserProductOwner,
-    isUserPoapOwner,
-    transactionHash,
-  };
-};
